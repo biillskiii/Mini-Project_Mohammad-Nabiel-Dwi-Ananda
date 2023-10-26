354501954f340c54f1595311d6feb1aa9c856024
biillskiii <muhamad.nabil20052003@gmail.com>
2023-10-26 18:24:02 +0700

    add preloader

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { CgSpinner } from "react-icons/cg";
+import { ImSpinner9 } from "react-icons/im";
 function Loading() {
   return (
-    <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-transparent bg-opacity-20 z-50">
-      <CgSpinner size={70} className="animate-spin" color="green"/>
+    <div className="bg-black fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center opacity-50 z-50">
+      <ImSpinner9 size={70} className="animate-spin" color="green"/>
       </div>
   );
 }
