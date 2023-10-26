e08b965498ba33ec0d80989dece0594ec725bc24
biillskiii <muhamad.nabil20052003@gmail.com>
2023-10-26 18:30:02 +0700

    add preloader

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ImSpinner9 } from "react-icons/im";
 function Loading() {
   return (
-    <div className="bg-black fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center opacity-50 z-50">
+    <div className="bg-black fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center opacity-95 z-50">
       <ImSpinner9 size={70} className="animate-spin" color="green"/>
       </div>
   );
